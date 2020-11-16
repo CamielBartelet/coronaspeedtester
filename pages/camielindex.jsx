d8028a3d0f9432881f217cef163576f045095299
Camiel Bartelet <c.bartelet@student.fontys.nl>
2020-11-16 14:29:34 +0100

    Integrate mongodb inside next environment

--- pages/camielindex.jsx
+++ pages/camielindex.jsx
@@ -1,11 +1,25 @@
 import HomeButton from "../appBuild/Components/homecomp/Homebutton";
+import useSWR from "swr";
+// import "../lib/server";
 
 const CamielProto = () => {
+  const baseUrl = "/api/users";
+  const users = useSWR(baseUrl).data?.Users;
+  console.log(users);
+
   return (
     <>
       <HomeButton />
       <main className="container">
         <p>Hi there Camiel</p>
+        <button
+          style={{ width: "100px", height: "25px" }}
+          onClick={() => {
+            JSON.stringify(users, null, 2);
+          }}
+        >
+          Show data
+        </button>
       </main>
     </>
   );
---
+++ pages/users.js
@@ -0,0 +1,37 @@
+import { connectToDatabase } from "../util/mongodb";
+
+export default function Top({ users }) {
+  return (
+    <div>
+      <h1>Top 1000 Movies of All Time</h1>
+      <p>
+        <small>(According to Metacritic)</small>
+      </p>
+      <ul>
+        {users.map((user) => (
+          <li>
+            <h2>{user.userName}</h2>
+            <h3>{user.userAge}</h3>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const { db } = await connectToDatabase();
+
+  const users = await db
+    .collection("users")
+    .find({})
+    .sort({ metacritic: -1 })
+    .limit(1000)
+    .toArray();
+
+  return {
+    props: {
+      users: JSON.parse(JSON.stringify(users)),
+    },
+  };
+}
