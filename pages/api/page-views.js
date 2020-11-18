bb3d86e29af94b972c652c0a48c50334a9918968
Camiel Bartelet <c.bartelet@student.fontys.nl>
2020-11-18 19:10:48 +0100

    Update page-views.js

--- pages/api/page-views.js
+++ pages/api/page-views.js
@@ -1,35 +1,35 @@
-import { google } from "googleapis";
+// import { google } from "googleapis";
 
-let auth;
-const googleEnvs = {
-  credentials: {
-    client_id: process.env.GOOGLE_CLIENT_ID,
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
-  },
-  scopes: ["https://www.googleapis.com/auth/analytics.readonly"],
-};
+// let auth;
+// const googleEnvs = {
+//   credentials: {
+//     client_id: process.env.GOOGLE_CLIENT_ID,
+//     client_email: process.env.GOOGLE_CLIENT_EMAIL,
+//     private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
+//   },
+//   scopes: ["https://www.googleapis.com/auth/analytics.readonly"],
+// };
 
-console.log(googleEnvs);
+// console.log(googleEnvs);
 
-export default async (_, res) => {
-  auth = new google.auth.GoogleAuth(googleEnvs);
+// export default async (_, res) => {
+//   auth = new google.auth.GoogleAuth(googleEnvs);
 
-  const analytics = google.analytics({
-    auth,
-    version: "v3",
-  });
-  const startDate = "2020-11-17";
+//   const analytics = google.analytics({
+//     auth,
+//     version: "v3",
+//   });
+//   const startDate = "2020-11-17";
 
-  // const startDate = req.query.startDate;
-  const response = await analytics.data.ga.get({
-    "end-date": "today",
-    ids: "ga:186371021",
-    metrics: "ga:pageviews",
-    "start-date": startDate,
-  });
+//   // const startDate = req.query.startDate;
+//   const response = await analytics.data.ga.get({
+//     "end-date": "today",
+//     ids: "ga:186371021",
+//     metrics: "ga:pageviews",
+//     "start-date": startDate,
+//   });
 
-  return res.status(200).json({
-    pageViews: response.data.rows[0][0],
-  });
-};
+//   return res.status(200).json({
+//     pageViews: response.data.rows[0][0],
+//   });
+// };
