e6582504fd702691616c0c3350c0386fa3735734
Camiel Bartelet <c.bartelet@student.fontys.nl>
2020-12-08 17:51:16 +0100

    Fixed export blanco page

--- pages/users.js
+++ pages/users.js
@@ -1,23 +1,23 @@
 // import dbConnect from "../util/mongodb";
 
-// export default function Top({ users }) {
-//   return (
-//     <div>
-//       <h1>Top 1000 Movies of All Time</h1>
-//       <p>
-//         <small>(According to Metacritic)</small>
-//       </p>
-//       <ul>
-//         {users.map((user) => (
-//           <li>
-//             <h2>{user.userName}</h2>
-//             <h3>{user.userAge}</h3>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
+export default function Top({ users }) {
+  return (
+    <div>
+      <h1>Top 1000 Movies of All Time</h1>
+      <p>
+        <small>(According to Metacritic)</small>
+      </p>
+      <ul>
+        {users.map((user) => (
+          <li>
+            <h2>{user.userName}</h2>
+            <h3>{user.userAge}</h3>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 // export async function getStaticProps() {
 //   const { db } = await dbConnect();
