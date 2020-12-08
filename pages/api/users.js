4b5be922e12f4834a9f5bc4d03ef34bddd1ffaa7
Camiel Bartelet <c.bartelet@student.fontys.nl>
2020-12-08 17:48:48 +0100

    Remove db to prevent error

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,14 +1,14 @@
-import dbConnect from "../../util/mongodb";
+// import dbConnect from "../../util/mongodb";
 
-export default async (req, res) => {
-  const { db } = await dbConnect();
+// export default async (req, res) => {
+//   const { db } = await dbConnect();
 
-  const users = await db
-    .collection("users")
-    .find({})
-    .sort({ metacritic: -1 })
-    .limit(20)
-    .toArray();
+//   const users = await db
+//     .collection("users")
+//     .find({})
+//     .sort({ metacritic: -1 })
+//     .limit(20)
+//     .toArray();
 
-  res.json(users);
-};
+//   res.json(users);
+// };
--- pages/users.js
+++ pages/users.js
@@ -1,37 +1,37 @@
-import dbConnect from "../util/mongodb";
+// import dbConnect from "../util/mongodb";
 
-export default function Top({ users }) {
-  return (
-    <div>
-      <h1>Top 1000 Movies of All Time</h1>
-      <p>
-        <small>(According to Metacritic)</small>
-      </p>
-      <ul>
-        {users.map((user) => (
-          <li>
-            <h2>{user.userName}</h2>
-            <h3>{user.userAge}</h3>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+// export default function Top({ users }) {
+//   return (
+//     <div>
+//       <h1>Top 1000 Movies of All Time</h1>
+//       <p>
+//         <small>(According to Metacritic)</small>
+//       </p>
+//       <ul>
+//         {users.map((user) => (
+//           <li>
+//             <h2>{user.userName}</h2>
+//             <h3>{user.userAge}</h3>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
-export async function getStaticProps() {
-  const { db } = await dbConnect();
+// export async function getStaticProps() {
+//   const { db } = await dbConnect();
 
-  const users = await db
-    .collection("users")
-    .find({})
-    .sort({ metacritic: -1 })
-    .limit(1000)
-    .toArray();
+//   const users = await db
+//     .collection("users")
+//     .find({})
+//     .sort({ metacritic: -1 })
+//     .limit(1000)
+//     .toArray();
 
-  return {
-    props: {
-      users: JSON.parse(JSON.stringify(users)),
-    },
-  };
-}
+//   return {
+//     props: {
+//       users: JSON.parse(JSON.stringify(users)),
+//     },
+//   };
+// }
