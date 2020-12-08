3cfbb9cd439a61cd24aee25eb1419687b090ab29
Nigel Ridderhof <387625@student.fontys.nl>
2020-12-08 10:31:59 +0100

    Eventix embed tested

--- pages/nigelindex.jsx
+++ pages/nigelindex.jsx
@@ -6,6 +6,10 @@ const NigelProto = () => {
       <HomeButton />
       <main className="container">
         <p>Hi there Nigel</p>
+
+        <div id="shop-frame" data-url="https://shop.eventix.io/0dfd8577-4569-4b74-9c2a-88480561c115" style={{width: '400px', margin: '0 auto'}}></div>
+        <script src="https://shop.eventix.io/build/integrate.js"></script>
+
       </main>
     </>
   );
