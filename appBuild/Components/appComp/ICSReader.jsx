c5eea770405d04ef2cd57b615a208eb598878bce
Alicia Berkers <advancedarcherofficial@gmail.com>
2021-04-13 16:47:51 +0200

    ICS Reader WIP

---
+++ appBuild/Components/appComp/ICSReader.jsx
@@ -0,0 +1,12 @@
+const ICS = () => {
+ const fs = require('fs')
+    fs.readFile('.../TestlocatieEindhoven.ics', 'utf-8', (err, data) => {
+    if (err) throw err;
+   // console.log(data);
+})
+return (
+    data
+);
+};
+
+export default ICS;
--- pages/cms/testlocation/[id]/TestAgenda.jsx
+++ pages/cms/testlocation/[id]/TestAgenda.jsx
@@ -1,14 +1,15 @@
 import { useRouter } from "next/router";
 import dbConnect from "../../../../util/mongodb";
 import Testlocation from "../../../../models/Testlocation";
-import FullCalendar from "../../../../appBuild/Components/appComp/fullcalendar";
+//import FullCalendar from "../../../../appBuild/Components/appComp/fullcalendar";
+import ICSReader from "../../../../appBuild/Components/appComp/ICSReader";
 
 
 
 
 const TestAgenda = ({ testOrg }) => {
   console.log(testOrg);
-
+  console.log(ICSReader.data);
   return (
     <>
       <div>De Agenda</div>
@@ -17,10 +18,11 @@ const TestAgenda = ({ testOrg }) => {
         <li>{testOrg.name}</li>
         <li>{testOrg.region}</li>
         <div>
-      <FullCalendar defaultView='dayGridMonth' />
-      <FullCalendar defaultView='timeGridWeek' />
-    </div>
-      </div>
+      {/* <FullCalendar defaultView='dayGridMonth' />
+      <FullCalendar defaultView='timeGridWeek' /> */}
+      <li>{ICSReader.data}</li>
+       </div>
+       </div>
     </>
   );
 };
